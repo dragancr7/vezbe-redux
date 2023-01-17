153269b04eede874741f9eecbcdbd9184eaf76fd
Dragan <dragankiki967@gmail.com>
2023-01-17 21:49:53 +0100

    2commit

---
+++ src/app/store/feature/userSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+
+export const userSlice = createSlice({
+    name:"users",
+    initialState:{ 
+        user: {
+            email: "",
+            username: ""
+        }
+    },
+    reducers:{
+        login:(state, action) => {
+            console.log(action);
+            state.user = action.payload;
+        },
+    },
+});
+ export const {login} = userSlice.actions;
+ export const selectUser = (state) => state.user.user;
+ export default userSlice.reducer;
